eef684aba8f3b0dfec7fd5a50ab7c4fe098bc8be
adnansadar <adnansadar11@gmail.com>
2020-02-11 12:51:52 +0530

    adding new program

--- Assignment/thread.c
+++ Assignment/thread.c
@@ -12,11 +12,11 @@ int main()
 	else
 		printf("Not Returned\n");
 	printf("I am in main\n");
-	//pthread_join(th,stat);
+	pthread_join(th,stat);
 }
 
 void *test()
 {
 	printf("I am in thread\n");
-	//pthread_exit(&stat);
+	pthread_exit(&stat);
 }
